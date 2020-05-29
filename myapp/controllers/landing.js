15f89d5083cf01b9ee9211fe123dd33283517733
Yiduo Ke <yk467@cornell.edu>
2020-05-29 15:19:42 -0400

    delete implemented

--- myapp/controllers/landing.js
+++ myapp/controllers/landing.js
@@ -18,7 +18,7 @@ exports.show_leads = function(req, res, next) {
         res.render('landing', { title: 'Express', leads: leads });
     })
 }
-
+;;;;
 exports.show_lead = function(req, res, next) {
     return models.Lead.findOne({
         where : {
@@ -26,5 +26,38 @@ exports.show_lead = function(req, res, next) {
         }
     }).then(lead => {
         res.render('lead', {lead:lead});
+    })
+}
+
+exports.show_edit_lead = function(req, res, next) {
+    return models.Lead.findOne({
+        where : {
+            id: req.params.lead_id
+        }
+    }).then(lead => {
+        res.render('lead/edit_lead', {lead:lead});
+    })
+}
+
+exports.edit_lead = function(req, res, next) {
+
+    return models.Lead.update({
+        email: req.body.lead_email
+    }, {
+        where:{
+            id : req.params.lead_id
+        }
+    }).then(result =>{
+        res.redirect('/lead/' + req.params.lead_id);
+    })
+}
+
+exports.delete_lead = function(req, res, next){
+    return models.Lead.destroy({
+        where: {
+            id: req.params.lead_id
+        }
+    }).then(result => {
+        res.redirect('/leads');
     })
 }
--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -8,4 +8,7 @@ router.get('/', landing.get_landing);
 router.post('/', landing.submit_lead)
 router.get('/leads', landing.show_leads);
 router.get('/lead/:lead_id', landing.show_lead);
+router.get('/lead/:lead_id/edit', landing.show_edit_lead);
+router.post('/lead/:lead_id/edit', landing.edit_lead);
+router.post('/lead/:lead_id/delete', landing.delete_lead);
 module.exports = router;
